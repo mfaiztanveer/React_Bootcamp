ca3b598947b7f3f04b19eb158f2ba54006764c88
mfaiztanveer <mfaiztan93@gmail.com>
2024-09-03 02:30:53 +0500

    Update footer layout and responsiveness

--- Project/src/components/footer/LowerFooter.jsx
+++ Project/src/components/footer/LowerFooter.jsx
@@ -1,72 +1,58 @@
-import React from 'react'
-import FooterLogo from "../../assets/imgs/logo.png"
-import "./Lowerfooter.css"
-import facebook from "../../assets/imgs/facebook.png"
-import X from "../../assets/imgs/twitter.png"
-import LinkedIn from "../../assets/imgs/linkedIn.png"
-import Insta from "../../assets/imgs/instagram.png"
-
+import React from 'react';
+import FooterLogo from "../../assets/imgs/logo.png";
+import "./LowerFooter.css";
+import facebook from "../../assets/imgs/facebook.png";
+import twitter from "../../assets/imgs/twitter.png";
+import linkedin from "../../assets/imgs/linkedIn.png";
+import instagram from "../../assets/imgs/instagram.png";
 
 export default function LowerFooter() {
   return (
-    <div>
-      <CompltFooter/>
-    </div>
-  )
-}
-
-
-function CompltFooter() {
-    return (
-        <div className='ft-main'>
-
-            <div className="ft-div">
-
-                <div className="Logo">
-                    <img src={FooterLogo} alt="x" />
-                    <div className='logo-text'>Insider's <span>Inventory</span></div>
-                    <h3>Your Gateway to Real Estate Excellence</h3>
-                </div>
-
-
-                <div className="listAndSocials">
-                    <List />
-
-                    <div className="ft-socials">
-
-                        <a href="https://www.facebook.com"><img src={facebook} alt="facebook" /></a>
-                        <a href="https://www.X.com"><img src={X} alt="X" /></a>
-                        <a href="https://www.linkedin.com"><img src={LinkedIn} alt="Linekdin" /></a>
-                        <a href="https://www.Instagram.com"><img src={Insta} alt="Instagram" /></a>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="rights">
-                <hr />
-                <p>All right are reserved For insider's Inventory</p>
-            </div>
-
+    <footer className='ft-main'>
+      <div className="ft-content">
+        <div className="logo-section">
+          <img src={FooterLogo} alt="Insider's Inventory Logo" className='footer-logo' />
+          <div className='logo-text'>Insider's <span>Inventory</span></div>
+          <h3>Your Gateway to Real Estate Excellence</h3>
         </div>
-    )
+        <div className="list-and-socials">
+          <List />
+          <div className="social-icons">
+            <a href="https://www.facebook.com" aria-label="Facebook" className="social-icon">
+              <img src={facebook} alt="Facebook" />
+            </a>
+            <a href="https://www.twitter.com" aria-label="Twitter" className="social-icon">
+              <img src={twitter} alt="Twitter" />
+            </a>
+            <a href="https://www.linkedin.com" aria-label="LinkedIn" className="social-icon">
+              <img src={linkedin} alt="LinkedIn" />
+            </a>
+            <a href="https://www.instagram.com" aria-label="Instagram" className="social-icon">
+              <img src={instagram} alt="Instagram" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="rights">
+        <hr />
+        <p>All rights are reserved for Insider's Inventory</p>
+      </div>
+    </footer>
+  );
 }
 
-
- function List() {
-    return (
-      <div className="nb-list">
+function List() {
+  return (
+    <div className="nav-list">
       <ul>
-          <li>Home</li>
-          <li>Buyers</li>
-          <li>Sellers</li>
-          <li>Buy & hold</li>
-          <li>Retail</li>
-          <li>Flip Opportunities</li>
-          <li>Off-Market Inventory</li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#buyers">Buyers</a></li>
+        <li><a href="#sellers">Sellers</a></li>
+        <li><a href="#buy-hold">Buy & Hold</a></li>
+        <li><a href="#retail">Retail</a></li>
+        <li><a href="#flip-opportunities">Flip Opportunities</a></li>
+        <li><a href="#off-market">Off-Market Inventory</a></li>
       </ul>
-  </div>
-  )
-  }
+    </div>
+  );
+}
--- Project/src/pages/OffMarket-Inventory/OffMarket.jsx
+++ Project/src/pages/OffMarket-Inventory/OffMarket.jsx
@@ -255,7 +255,7 @@ export default function OffMarket() {
                 <section className="padding">
                 <Grid container spacing={3}>
                 {sortedAndPagedItems().map((item, index) => (
-                        <Grid  xs={12} sm={6} md={4} lg={4} key={index}>
+                        <Grid  xs={12} sm={6} md={4} lg={4} key={index} className="properties-grid">
                             <Card
                                 image={item.image}
                                 status={item.status}
